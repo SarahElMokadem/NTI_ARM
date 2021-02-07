2e86750a6d99542579878765de92a3b82b89fb28
DESKTOP-U4156L0\Sarah <saramokadem4@gmail.com>
2021-02-08 01:30:47 +0200

    7Segment_Driver

--- HAL/7Segment/SSeg_config.h
+++ HAL/7Segment/SSeg_config.h
@@ -11,25 +11,36 @@
 /* Choose between 1-COMMON_CATHODE
  *                2-COMMON_ANODE
  */
-#define      SSEG_COMMON_TYPE     COMMON_CATHODE
+#define      SSEG_COMMON_TYPE     COMMON_ANODE
 
 /* Choose number of Segments u want to use 1- 1
  *                                         2- 2
  *
  */
-#define      SSEG_NUMBERS         1
+#define      SSEG_NUMBERS         2
 
-
+/******************************        IN CASE 2 SSEG                    *****************************/
 /* In case 2 Seven segment choose which ports*/
-#define      SSEG1_NUMBERS         DIO_U8_PORTA
-#define      SSEG2_NUMBERS         DIO_U8_PORTB
+/* Choose Which Part of the Port 1-LOW
+ *                               2-HIGH
+ */
+#define      SSEG1_PORT         DIO_U8_PORTA
+#define      SSEG1_PART            LOW
+#define      SSEG2_PORT         DIO_U8_PORTB
+#define      SSEG2_PART            HIGH
 
+
+
+/********************************    IN CASE 1 SSEG                    **************************/
 /* Choose Which Port 1- DIO_U8_PORTA
  *                   2- DIO_U8_PORTB
  *                   3- DIO_U8_PORTC
  */
+/* Choose Which Part of the Port 1-LOW
+ *                               2-HIGH
+ */
 #define      SSEG_PORT            DIO_U8_PORTA
-
+#define      SSEG_PART            LOW
 
 
 #endif /* SSEG_CONFIG_H_ */
--- HAL/7Segment/SSeg_program.c
+++ HAL/7Segment/SSeg_program.c
@@ -12,6 +12,8 @@
 #include "SSeg_interface.h"
 #include "SSeg_config.h"
 
+
+
 /*Array to Display the numbers and the Hex numbers*/
 static u8 segCathode[]={0x3F,0x06,0x5B,0x4F,0x66,0x6D,0x7D,0x07,0x7F,0x6F};
 static u8 segCathodeHEX[]={0x3F,0x06,0x5B,0x4F,0x66,0x6D,0x7D,0x07,0x7F,0x6F,0x77,0x7C,0x39,0x5E,0x79,0x71};
@@ -22,57 +24,59 @@ static u8 segAnodeHEX[]={0x3F,0x06,0x5B,0x4F,0x66,0x6D,0x7D,0x07,0x7F,0x6F,0x77,
 
 #if(SSEG_COMMON_TYPE==COMMON_CATHODE)
 
-  #if(SSEG_NUMBERS==1)
+#if(SSEG_NUMBERS==1)
 
-  void SSEG_VoidDisplayNumber(u8 number)
-  {
-  	DIO_voidSetPortValue(SSEG_PORT,segCathode[number]);
-  }
+void SSEG_VoidDisplayNumber(u8 number)
+{
+	DIO_voidSetPortValue(SSEG_PORT,SSEG_PART,segCathode[number]);
+}
 
-  void SSEG_VoidDisplayHex(u8 number)
-  {
-  	DIO_voidSetPortValue(SSEG_PORT,segCathodeHEX[number]);
-  }
-  #elif(SSEG_NUMBERS==2)
-  void SSEG_VoidDisplayNumber(u8 number)
-  {
-  	DIO_voidSetPortValue(SSEG1_PORT,segCathode[number/10]);
-  	DIO_voidSetPortValue(SSEG2_PORT,segCathode[number%10]);
-  }
+void SSEG_VoidDisplayHex(u8 number)
+{
+	DIO_voidSetPortValue(SSEG_PORT,SSEG_PART,segCathodeHEX[number]);
+}
+#elif(SSEG_NUMBERS==2)
 
-  void SSEG_VoidDisplayHex(u8 number)
-  {
-  	DIO_voidSetPortValue(SSEG1_PORT,segCathodeHEX[number%10]);
-  	DIO_voidSetPortValue(SSEG2_PORT,segCathodeHEX[number%10]);
-  }
-  #endif
+void SSEG_VoidDisplayNumber(u8 number)
+{
+	DIO_voidSetPortValue(SSEG1_PORT,SSEG1_PART,segCathode[number/10]);
+	DIO_voidSetPortValue(SSEG2_PORT,SSEG2_PART,segCathode[number%10]);
+}
+
+void SSEG_VoidDisplayHex(u8 number)
+{
+	DIO_voidSetPortValue(SSEG1_PORT,SSEG1_PART,segCathodeHEX[number/10]);
+	DIO_voidSetPortValue(SSEG2_PORT,SSEG2_PART,segCathodeHEX[number%10]);
+}
+#endif
 
 #elif(SSEG_COMMON_TYPE==COMMON_ANODE)
 
-  #if(SSEG_NUMBERS==1)
+#if(SSEG_NUMBERS==1)
 
-  void SSEG_VoidDisplayNumber(u8 number)
-  {
-  	DIO_voidSetPortValue(SSEG_PORT,segAnode[number]);
-  }
+void SSEG_VoidDisplayNumber(u8 number)
+{
+	DIO_voidSetPortValue(SSEG_PORT,SSEG_PART,segAnode[number]);
+	DIO_voidSetPortValue(SSEG_PORT,SSEG_PART,segAnode[number]);
+}
 
-  void SSEG_VoidDisplayHex(u8 number)
-  {
-  	DIO_voidSetPortValue(SSEG_PORT,segAnodeHEX[number]);
-  }
-  #elif(SSEG_NUMBERS==2)
+void SSEG_VoidDisplayHex(u8 number)
+{
+	DIO_voidSetPortValue(SSEG_PORT,SSEG_PART,~segAnodeHEX[number]);
+}
+#elif(SSEG_NUMBERS==2)
 
-  void SSEG_VoidDisplayNumber(u8 number)
-  {
-  	DIO_voidSetPortValue(SSEG1_PORT,segAnode[number/10]);
-  	DIO_voidSetPortValue(SSEG2_PORT,segAnode[number%10]);
-  }
+void SSEG_VoidDisplayNumber(u8 number)
+{
+	DIO_voidSetPortValue(SSEG1_PORT,SSEG1_PART,segAnode[number/10]);
+	DIO_voidSetPortValue(SSEG2_PORT,SSEG2_PART,segAnode[number%10]);
+}
 
-  void SSEG_VoidDisplayHex(u8 number)
-  {
-  	DIO_voidSetPortValue(SSEG1_PORT,~segAnodeHEX[number%10]);
-  	DIO_voidSetPortValue(SSEG2_PORT,~segAnodeHEX[number%10]);
-  }
-  #endif
+void SSEG_VoidDisplayHex(u8 number)
+{
+	DIO_voidSetPortValue(SSEG1_PORT,SSEG1_PART,~segAnodeHEX[number/10]);
+	DIO_voidSetPortValue(SSEG2_PORT,SSEG2_PART,~segAnodeHEX[number%10]);
+}
+#endif
 
 #endif

6c1d2b78676410744f85f00dc80c2a02e00a9922
DESKTOP-U4156L0\Sarah <saramokadem4@gmail.com>
2021-02-10 15:42:27 +0200

    Libirary files

---
+++ LIB/StdTypes.h
@@ -0,0 +1,29 @@
+/*
+ * StdTypes.h
+ *
+ * Created: 12/1/2020 11:21:54 AM
+ *  Author: Sarah
+ */
+
+
+#ifndef STDTYPES_H_
+#define STDTYPES_H_
+
+typedef      unsigned char            u8;
+typedef      signed char              s8;
+typedef      unsigned int             u16;
+typedef      signed int               s16;
+typedef      unsigned long            u32;
+typedef      signed long              s32;
+typedef      unsigned long long       u64;
+typedef      signed long long         s64;
+
+
+#ifndef    NULL
+#define    NULL       (void *)0
+#endif
+
+#define    NULLPTR  0
+
+
+#endif /* STDTYPES_H_ */
